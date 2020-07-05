a2ae17c2b19b22fb7f8f44f049dcb18df0713aa7
tiangua <tiangua@qunhemail.com>
2020-07-05 16:23:52 +0800

    完成#2、#19

---
+++ src/main/java/com/qunhe/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,16 @@
+package com.qunhe.leetcode.editor.cn;
+
+/**
+ * Function:
+ *
+ * @author tiangua
+ * @date 2020/7/5
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
