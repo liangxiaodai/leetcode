abad127bcf9ea45655c67a49152c1b827a44c9d7
tiangua <tiangua@qunhemail.com>
2021-03-04 22:29:50 +0800

    归并两个有序的链表

--- src/main/java/com/qunhe/leetcode/editor/cn/P21MergeTwoSortedLists.java
+++ src/main/java/com/qunhe/leetcode/editor/cn/P21MergeTwoSortedLists.java
@@ -59,10 +59,10 @@ public class P21MergeTwoSortedLists {
             }
             if (l1.val < l2.val) {
                 l1.next = mergeTwoLists(l1.next, l2);
-                return l1;
+                return l1;//返回小的结点
             } else {
                 l2.next = mergeTwoLists(l1, l2.next);
-                return l2;
+                return l2;//返回小的结点
             }
         }
     }
