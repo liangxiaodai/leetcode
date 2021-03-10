93fd8c14d5bc3d2ea2dc43521b7ce94ad0287dbc
tiangua <tiangua@qunhemail.com>
2021-03-10 14:11:02 +0800

    104 树的高度

---
+++ src/main/java/com/qunhe/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,19 @@
+package com.qunhe.leetcode.editor.cn;
+
+/**
+ * Function: 树的结点
+ *
+ * @author tiangua
+ * @date 2021/3/9
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
