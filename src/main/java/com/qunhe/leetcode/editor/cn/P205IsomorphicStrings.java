79631db7924b353276f42e9260f3cc2924e781ca
tiangua <44131059+liangxiaodai@users.noreply.github.com>
2021-04-13 15:37:32 +0800

    205 字符串同构

---
+++ src/main/java/com/qunhe/leetcode/editor/cn/P205IsomorphicStrings.java
@@ -0,0 +1,60 @@
+package com.qunhe.leetcode.editor.cn;
+//给定两个字符串 s 和 t，判断它们是否是同构的。
+//
+// 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
+//
+// 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入：s = "egg", t = "add"
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "foo", t = "bar"
+//输出：false
+//
+// 示例 3：
+//
+//
+//输入：s = "paper", t = "title"
+//输出：true
+//
+//
+//
+// 提示：
+//
+//
+// 可以假设 s 和 t 长度相同。
+//
+// Related Topics 哈希表
+// 👍 347 👎 0
+
+public class P205IsomorphicStrings {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isIsomorphic(String s, String t) {
+            int[] preIndexOfS = new int[256];
+            int[] preIndexOfT = new int[256];
+            for (int i = 0; i < s.length(); i++) {
+                char sc = s.charAt(i);
+                char tc = t.charAt(i);
+                if (preIndexOfS[sc] != preIndexOfT[tc]) {
+                    return false;
+                }
+                //记录一个字符上次出现的位置
+                preIndexOfS[sc] = i + 1;
+                preIndexOfT[tc] = i + 1;
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
